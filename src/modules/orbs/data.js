2063ed2cf5b5c7522ada4afb4f4e14a53384db0a
Michi <freimichael3@gmail.com>
2024-12-11 16:22:12 +0100

    fix orbs npc

--- src/modules/orbs/data.js
+++ src/modules/orbs/data.js
@@ -2,10 +2,13 @@ module.exports = {
     chatPatterns: {
         success: /^\[Orbs\] Du hast erfolgreich ([\d\.,]+) \S+ für ([\d\.,]+) Orbs verkauuft\.$/,
     },
+    windowPatterns: {
+        orbsMenu: /^Händler$/
+    },
     npc: {
         identifier: 'Händler',
         position: [172, 25, -42],
         world: 'orbs',
-        onInteract: 'windowOpen:lottery->menu',
+        onInteract: 'windowOpen:orbs->orbsMenu',
     }
 }
