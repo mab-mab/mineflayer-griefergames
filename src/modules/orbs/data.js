8e3c9731cc720b9155c99848b851cad179fa9104
Michi <freimichael3@gmail.com>
2024-12-18 17:29:49 +0100

    add more item patterns

--- src/modules/orbs/data.js
+++ src/modules/orbs/data.js
@@ -16,6 +16,10 @@ module.exports = {
         orbBalance:{
             name: 'skull', // The name of the item (a skull in this case)
             customName: '§6Deine Orbs'     // The custom name you want to match (without color codes)
+        },
+        back:{
+            name: 'skull', // The name of the item (a skull in this case)
+            customName: '§6Zurück zur vorherigen Seite'     // The custom name you want to match (without color codes)
         }
     }
 }
